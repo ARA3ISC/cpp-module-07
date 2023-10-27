8b48ad0c05d42632143791a09dd8936285e66121
arabisc <m.aneddame01@gmail.com>
2023-10-27 18:17:24 +0200

    passing by ref instead of passing by value

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -13,13 +13,13 @@ template <typename T> void swap(T &a, T &b)
 }
 
 template <typename T>
-T min(T a, T b)
+T min(const T& a, const T& b)
 {
 	return (a < b) ? a : b;
 }
 
 template <typename T>
-T max(T a, T b)
+T max(const T& a, const T &b)
 {
 	return (a > b) ? a : b;
 }
